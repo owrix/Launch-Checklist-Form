494c7d0a891140332f6ccf6eca800482328a647e
Your Name <you@example.com>
2021-03-29 14:03:46 -0500

    added planet destination randomizer

--- script.js
+++ script.js
@@ -41,17 +41,18 @@ window.addEventListener("load", function() {
    this.fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
       response.json().then(function(json) {
          const div = document.getElementById("missionTarget");
+         const randomDestination = Math.floor(Math.random() * 6);
          div.innerHTML =
          `
          <h2>Mission Destination</h2>
          <ol>
-            <li>Name: ${json[0].name}</li>
-            <li>Diameter: ${json[0].diameter}</li>
-            <li>Star: ${json[0].star}</li>
-            <li>Distance from Earth: ${json[0].distance}</li>
-            <li>Number of Moons: ${json[0].moons}</li>
+            <li>Name: ${json[randomDestination].name}</li>
+            <li>Diameter: ${json[randomDestination].diameter}</li>
+            <li>Star: ${json[randomDestination].star}</li>
+            <li>Distance from Earth: ${json[randomDestination].distance}</li>
+            <li>Number of Moons: ${json[randomDestination].moons}</li>
          </ol>
-         <img src="${json[0].image}"></img>
+         <img src="${json[randomDestination].image}"></img>
          `
       })
    })
